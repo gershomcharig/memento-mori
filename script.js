2ceccd799f07be2b9b7cdbccfcfc2b8076cab42f
gershomcharig <gershom.charig@gmail.com>
2022-05-09 16:05:44 +0100

    Added the calendar!!!

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 // Global variables
 const dateOfBirth = new Date(1988, 7, 22);
+const lifeExpectancy = 90;
 const now = new Date();
 
 const msPerMinute = 60 * 1000;
@@ -11,16 +12,25 @@ const msPerMonth = msPerDay * 30;
 const msPerYear = msPerDay * 365;
 
 const yearsLived = (now - dateOfBirth) / msPerYear;
-const yearsRemaining = 90 - yearsLived;
-const yearsLivedPerc = (yearsLived / 90) * 100;
-const yearsRemainingPerc = (yearsRemaining / 90) * 100;
+const yearsRemaining = lifeExpectancy - yearsLived;
+const yearsLivedPerc = (yearsLived / lifeExpectancy) * 100;
+const yearsRemainingPerc = (yearsRemaining / lifeExpectancy) * 100;
 
-// UI
+// UI classes
 const labelDateOfBirth = document.querySelector(".dateOfBirth");
 const labelYearsLived = document.querySelector(".yearsLived");
 const labelYearsRemaining = document.querySelector(".yearsRemaining");
+
+const calendar = document.querySelector(".calendar");
+const calendarCell = document.querySelector(".calendar--cell");
 
 // Build the UI
 labelDateOfBirth.textContent = `${new Intl.DateTimeFormat("en-GB", { dateStyle: "medium" }).format(dateOfBirth)}`;
 labelYearsLived.textContent = `${yearsLived.toFixed(1)} (${yearsLivedPerc.toFixed(1)}%)`;
 labelYearsRemaining.textContent = `${yearsRemaining.toFixed(1)} (${yearsRemainingPerc.toFixed(1)}%)`;
+
+for (let i = 0; i < 52 * lifeExpectancy; i++) {
+  i <= yearsLived * 52
+    ? calendar.insertAdjacentHTML("beforeend", `<div class="calendar--cell filled"></div>`)
+    : calendar.insertAdjacentHTML("beforeend", `<div class="calendar--cell"></div>`);
+}
